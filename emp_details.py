37a3b336b7ce9b7c454cb2818f25ef3faf1ae8e0
veereshsajjansajjan1-svg <veereshsajjansajjan1@gmail.com>
2025-11-20 15:32:53 +0530

    Update emp_details.py

--- emp_details.py
+++ emp_details.py
@@ -2,10 +2,10 @@ import sys
 if len(sys.argv) != 4:
     print("Usage: python employedetails.py <name> <rollno> <id> <salary>")
     sys.exit(1)
-name =  sys.argv[1]
-experience = sys.argv[2]
-id = sys.argv[3]
-salary = sys.argv[4]
+name =  sys.argv[0]
+experience = sys.argv[1]
+id = sys.argv[2]
+salary = sys.argv[3]
 
 print("employee name is",name)
 print("experience is",experience)
